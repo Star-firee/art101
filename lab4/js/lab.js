f7f6c444b41e0e8474799f13c720842397d4d91f
Elizabeth <Gotita152002@gmail.com>
2024-04-22 14:32:54 -0700

    lab4 code

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,28 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// An implementation of a game
+// ping pong
+// Elizabeth Itzel Tenorio
+// April 20th 2024
+
+
+//Creating a pong game:
+  //Generate a game space
+  //Generate score board
+  //Generate Boundary lines
+  //Generate center divider
+//Decide if there is one player or two players
+  //If one player, then second paddle is played by robot
+//Display round number
+//Generate 2 paddles, one on each side 
+  //One red
+  //One blue
+//Generate ball
+  //Ball moves in the right direction first
+  //Bounces off paddle
+//Computer evaluate ball position
+//Computer decides where to move paddle 
+//If paddle misses ball end round
+  //Point goes to opposite player
+//After third round, player with most points wins
 
 // Constants
 
