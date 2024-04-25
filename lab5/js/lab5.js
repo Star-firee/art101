69f3648e478857a556622e57494dbf017a2ce273
Elizabeth <Gotita152002@gmail.com>
2024-04-25 14:30:02 -0700

    lab 5 first run upload

---
+++ lab5/js/lab5.js
@@ -0,0 +1,19 @@
+// lab 5 creating data types and variables
+// Author: Elizabeth Tenorio
+// Date: 23 April 2024
+
+// Variables
+make= "chevrolet";
+model= "malibu";
+color="silver";
+year= 2017; 
+
+//calculate
+age = 2024 - year; 
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years <br>");
