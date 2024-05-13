f5a61fb9808cad281ffd30f70d95d3306e49b90f
Elizabeth <Gotita152002@gmail.com>
2024-05-12 17:10:30 -0700

    gosh darn divs and classes

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -2,13 +2,13 @@
 //Author: Elizabeth Tenorio
 //date: 11th May 2024
 
-//create a button to put in the challenge section of the html index 
-$(".minor-section").append("<button id='button-challenge'>click here!</button>");
+//create a button to put in the minor sections of the html index 
+$(".minor-section").append("<button class='button-challenge'>click here!</button>");
 
+//function will toggle the minor section class to utilize the special css 
 
-function turnLavender(){
-  $(".minor-section").toggleClass("special");
-}
-
-$(".minor-section").click(turnLavender)
-
+// add a click listener to the challenge button
+$(".button-challenge").click(function(){
+  // now add (or subtract) the "special" class to the section
+  $(this).parent().toggleClass("special");
+});
