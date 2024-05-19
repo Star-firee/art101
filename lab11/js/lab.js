9c234d0bb1de085abe03e3bc65e28d34e8342ef9
Elizabeth <Gotita152002@gmail.com>
2024-05-18 21:20:55 -0700

    updated <p>

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -7,18 +7,19 @@ function sortString(inputString) {
   // We have to convert our string to an array and back again to sort it
   return inputString.split('').sort().join('');
 }
-
+ 
 // click listener for button
 $("#submit").click(function(){
-
+  
   // get value of input field
   const userName = $("#user-name").val();
+           
    // now let's sort it
-  userNameSorted = sortString(userName);
+  userNameSorted = sortString(userName)
+  
 
    // append a new div to our output div
-   $("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
+   $("#output").html('<div class="text"><p>' + "Your name isss "+ userNameSorted + '</p></div>');
 });
-
 
 
